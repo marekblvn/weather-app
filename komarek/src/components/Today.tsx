2823031b69a8996a402bd8a9cb666f1aa63e5916
Marek Balvín <marek267balvin@gmail.com>
2025-03-11 01:28:27 +0100

    Cleaned Today component

--- komarek/src/components/Today.tsx
+++ komarek/src/components/Today.tsx
@@ -1,4 +1,6 @@
-import { Box, Grid2, Icon, Stack, Typography } from "@mui/material";
+import { Box, Grid2, Stack, Typography } from "@mui/material";
+import { getCurrentByLanAndLon } from "../services/api";
+import useGet from "../hooks/useGet";
 
 const daysShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const months = [
@@ -16,62 +18,11 @@ const months = [
   "December",
 ];
 
-const data = {
-  location: {
-    name: "Prague",
-    region: "Hlavni mesto Praha",
-    country: "Czech Republic",
-    lat: 50.083,
-    lon: 14.467,
-    tz_id: "Europe/Prague",
-    localtime_epoch: 1741650166,
-    localtime: "2025-03-11 00:42",
-  },
-  current: {
-    last_updated_epoch: 1741649400,
-    last_updated: "2025-03-11 00:30",
-    temp_c: 10.2,
-    temp_f: 50.4,
-    is_day: 0,
-    condition: {
-      text: "Light rain",
-      icon: "//cdn.weatherapi.com/weather/64x64/night/296.png",
-      code: 1183,
-    },
-    wind_mph: 8.9,
-    wind_kph: 14.4,
-    wind_degree: 254,
-    wind_dir: "WSW",
-    pressure_mb: 1001,
-    pressure_in: 29.56,
-    precip_mm: 0,
-    precip_in: 0,
-    humidity: 71,
-    cloud: 0,
-    feelslike_c: 8.2,
-    feelslike_f: 46.8,
-    windchill_c: 8.2,
-    windchill_f: 46.8,
-    heatindex_c: 10.2,
-    heatindex_f: 50.3,
-    dewpoint_c: 7,
-    dewpoint_f: 44.5,
-    vis_km: 10,
-    vis_miles: 6,
-    uv: 0,
-    gust_mph: 14.2,
-    gust_kph: 22.9,
-  },
-};
-
-const loading = false;
-const error = null;
-
 function Today() {
-  //   const { data, loading, error, refetch } = useGet(getCurrentByLanAndLon, {
-  //     lat: 50.075539,
-  //     lon: 14.4378,
-  //   });
+  const { data, loading, error } = useGet(getCurrentByLanAndLon, {
+    lat: 50.075539,
+    lon: 14.4378,
+  });
 
   function formatDate(d: string) {
     const date = new Date(d);
